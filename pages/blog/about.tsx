73f4f3526342992fe4767cec30adf0c9abe9f72f
wesleylcf <47099887+wesleylcf@users.noreply.github.com>
2021-07-09 16:56:18 +0800

    fix mobile in pages/blog/about

--- pages/blog/about.tsx
+++ pages/blog/about.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import About from '../../containers/blog/About/About';
 
-const index = ({ isDarkMode }) => {
+const index = ({ isDarkMode, isMobile }) => {
   return (
     <>
-      <About darkMode={isDarkMode} />
+      <About darkMode={isDarkMode} isMobile={isMobile} />
     </>
   );
 };
