88e39b6219e1f462cad947b6306680e8b40d4b71
wesleylcf <wesleylinzefeng@gmail.com>
2022-03-07 17:14:32 +0800

    add resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,7 +4,7 @@ const resume = () => {
   if (typeof window === "undefined") return <p>loading...</p>;
   return (
     <iframe
-      src="https://drive.google.com/file/d/1KHls66APdiny_UBKsZtXyp3YA-ywSGoq/preview"
+      src="https://drive.google.com/file/d/1TdARTQPqUFXAXzvlao-MXkrxppz-AX61/preview"
       width="80%"
       height={0.8 * window.innerHeight}
       allow="autoplay"
