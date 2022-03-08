1b2d96d24a44bd3de24415e91fed11dc864ae002
wesleylcf <47099887+wesleylcf@users.noreply.github.com>
2022-03-08 22:23:34 +0800

    update gitignore

---
+++ next.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    NOTION_KEY: "secret_vvPvjPxDwqx5zvM564Va1UzgRzoMt8aCJhopbuJaeGj",
+    NOTION_BLOG_DATABASE_ID: "1a1928b159f74dfebd5c81bf7bb5d856",
+    NOTION_PROJECTS_DATABASE_ID: "19c6efbc027243c78dcaed54c8e01b79",
+    NOTION_DOCS_DATABASE_ID: "cf2b1f796fb1459596fbf13c2d02c2a4",
+  },
+  images: {
+    domains: [
+      "www.flaticon.com",
+      "img.flaticon.com",
+      "logos-download.com",
+      "s3.us-west-2.amazonaws.com",
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/blog/about",
+        permanent: true,
+      },
+    ];
+  },
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+};
